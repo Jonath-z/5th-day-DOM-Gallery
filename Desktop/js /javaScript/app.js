eff6d286eba88764de365bab18d7fd2294a253f8
Jonathan-zihindula <jonathanzihindula95@gmail.com>
2021-03-25 19:38:00 +0200

    doc

---
+++ Desktop/js /javaScript/app.js
@@ -0,0 +1,12 @@
+var nme = document.getElementById('name');
+var date = document.getElementById('date');
+var emount = document.getElementById('emount');
+var button = document.getElementById('button');
+var titles = document.getElementById('title');
+
+nme.addEventListener('submit', function (e) {
+    console.log(nme);
+    e.preventDefault();
+})
+
+
